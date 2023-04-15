5fbdce1e7a57acc06b37c68c41ba919cbfe236d9
Brian <nieh_brian@yahoo.com>
2023-04-15 00:04:58 -0700

    displayBoard

--- main.cpp
+++ main.cpp
@@ -6,6 +6,26 @@ class TicTacToe
 public:
     void displayBoard()
     {
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i] == 1)
+            {
+                std::cout << 'O';
+            }
+            if (board[i] == 2)
+            {
+                std::cout << 'X';
+            }
+            if (board[i] == 0)
+            {
+                std::cout << '#';
+            }
+
+            if (i % 3 == 0)
+            {
+                std::cout << '\n';
+            }
+        }
     }
 
     // returns int based on state of game
